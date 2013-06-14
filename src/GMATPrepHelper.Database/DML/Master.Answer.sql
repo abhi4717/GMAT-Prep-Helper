93db254b93c373cf7588a76d5a75e12ae418d89f
Abhishek Gopalan <abhi4717@gmail.com>
2013-06-14 19:22:27 +0530

    Added Master.Answers.sql. File contains the master table for Answers table

---
+++ src/GMATPrepHelper.Database/DML/Master.Answer.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS(SELECT ID FROM Answers)
+BEGIN
+	SET IDENTITY_INSERT [Answers] ON;
+	INSERT INTO Answers([Id],[Option]) VALUES(1,'A');
+	INSERT INTO Answers([Id],[Option]) VALUES(2,'B');
+	INSERT INTO Answers([Id],[Option]) VALUES(3,'C');
+	INSERT INTO Answers([Id],[Option]) VALUES(4,'D');
+	INSERT INTO Answers([Id],[Option]) VALUES(5,'E');
+	SET IDENTITY_INSERT [Answers] OFF;
+END
