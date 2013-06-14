566883cdd1ecb9f43c4b9bd060937ca701e539cd
Abhishek Gopalan <abhi4717@gmail.com>
2013-06-14 19:05:11 +0530

    Added QuestionAnswer Table

---
+++ src/GMATPrepHelper.Database/DDL/QuestionAnswers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[QuestionAnswers]
+(
+	[Id] INT NOT NULL,
+	[Question_No] INT NOT NULL,
+	[Selected_Ans] INT NOT NULL,
+	[Correct_Ans] INT NULL,
+	[Remarks] VARCHAR(500) NULL,
+	CONSTRAINT pk_QuestionAnswer PRIMARY KEY([Id]),
+	CONSTRAINT fk_Answers_SelectedAns FOREIGN KEY([Selected_Ans]) REFERENCES Answers,
+	CONSTRAINT fk_Answers_CorrectAns FOREIGN KEY([Correct_Ans]) REFERENCES Answers
+)
