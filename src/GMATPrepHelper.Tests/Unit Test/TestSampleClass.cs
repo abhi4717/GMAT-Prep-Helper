0301c02c50644163979a7b04828b6a8fd4425132
Abhishek Gopalan <abhi4717@gmail.com>
2013-08-24 21:59:45 +0530

    Added msbuild scripts

---
+++ src/GMATPrepHelper.Tests/Unit Test/TestSampleClass.cs
@@ -0,0 +1,21 @@
+﻿
+using NUnit.Framework;
+namespace GMATPrepHelper.Tests.Unit_Test
+{
+    [TestFixture]
+    class TestSampleClass
+    {
+        [TestCase]
+        public void sample_test_case()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Ignore]
+        public void sample_fail_test_case()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
