caf5f7bd3b95e10cb3abea8b2c4c11b2d8d91a4f
Abhishek Gopalan <abhi4717@gmail.com>
2013-08-25 21:49:39 +0530

    Home page created

---
+++ src/GMATPrepHelper/Scripts/Home/Home.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    /// <summary>define the event bindings</summary>
+    $('#btnBeginSession').on('click', functions.beginSession);
+});
+
+
+var functions = {
+    beginSession: function() {
+        console.log('Begin Session');
+    }
+};
